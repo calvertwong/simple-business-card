a5e2288200f99d6d12d2518cfab5a1e01f831898
Calvert <wongcalvert@gmail.com>
2020-09-28 01:07:54 -0500

    Added code to local keystore for local build

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,12 +1,39 @@
+import java.util.Properties
+import java.io.FileInputStream
+
+// Create a variable called keystorePropertiesFile, and initialize it to your
+// keystore.properties file, in the rootProject folder.
+private val keystorePropertiesFile: File = rootProject.file("keystore.properties")
+var keystoreProperties: Properties? = null
+
+if (keystorePropertiesFile.exists()) {
+    // Initialize a new Properties() object called keystoreProperties.
+    keystoreProperties = Properties()
+
+    // Load your keystore.properties file into the keystoreProperties object.
+    keystoreProperties?.load(FileInputStream(keystorePropertiesFile))
+}
+
 android {
+    signingConfigs {
+        register("release") {
+            keyAlias = keystoreProperties?.get("keyAlias") as String
+            keyPassword = keystoreProperties?.get("keyPassword") as String
+            storeFile = file(keystoreProperties?.get("storeFile") as String)
+            storePassword = keystoreProperties?.get("storePassword") as String
+        }
+    }
+
     buildTypes {
         getByName("release") {
             isMinifyEnabled = true
             isShrinkResources = true
+            signingConfig = signingConfigs.getByName("release")
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
 }
+
 
 dependencies {
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
@@ -33,7 +60,7 @@ dependencies {
     implementation(Deps.DebugDrawer.debugDrawerOkhttp)
     implementation(Deps.DebugDrawer.debugDrawerOkhttp3)
     implementation(Deps.DebugDrawer.debugDrawerTimber)
-    
+
     debugImplementation(Deps.Chuck.chuckLibrary)
     releaseImplementation(Deps.Chuck.chuckLibraryNoOp)
 
